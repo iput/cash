582acff210f72ca04a82e08e313ed37116289863
Kang-Mahfud\Zamhari <makhfudzamhari@gmail.com>
2017-03-02 14:47:09 +0700

    database

--- petty_cash.sql
+++ petty_cash.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 24 Feb 2017 pada 21.17
+-- Generation Time: 02 Mar 2017 pada 08.46
 -- Versi Server: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -23,10 +23,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tabel_anggaran_pengeluaran`
+-- Struktur dari tabel `anggaran_pengeluaran`
 --
 
-CREATE TABLE IF NOT EXISTS `tabel_anggaran_pengeluaran` (
+CREATE TABLE IF NOT EXISTS `anggaran_pengeluaran` (
 `id_anggaran_pengeluaran` int(11) NOT NULL,
   `id_project` int(11) NOT NULL,
   `nama_pengeluaran` varchar(100) DEFAULT NULL,
@@ -37,10 +37,10 @@ CREATE TABLE IF NOT EXISTS `tabel_anggaran_pengeluaran` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tabel_level_user`
+-- Struktur dari tabel `level_user`
 --
 
-CREATE TABLE IF NOT EXISTS `tabel_level_user` (
+CREATE TABLE IF NOT EXISTS `level_user` (
 `id_level` int(11) NOT NULL,
   `nama_level` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -48,41 +48,29 @@ CREATE TABLE IF NOT EXISTS `tabel_level_user` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tabel_pendapatan_pribadi`
+-- Struktur dari tabel `pengeluaran_pribadi`
 --
 
-CREATE TABLE IF NOT EXISTS `tabel_pendapatan_pribadi` (
-`id_pendapatan_p` int(11) NOT NULL,
-  `nama_pendapatan` varchar(100) DEFAULT NULL,
-  `tanggal_pemasukan` datetime DEFAULT NULL,
-  `id_user` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `tabel_pengeluaran_pribadi`
---
-
-CREATE TABLE IF NOT EXISTS `tabel_pengeluaran_pribadi` (
+CREATE TABLE IF NOT EXISTS `pengeluaran_pribadi` (
 `id_pengeluaran_pribadi` int(11) NOT NULL,
-  `id_saldo_pribadi` int(11) NOT NULL,
-  `nama_pengeluaran` varchar(100) DEFAULT NULL,
-  `jumlah_pengeluaran` double DEFAULT NULL,
-  `waktu_pengeluaran` datetime DEFAULT NULL,
-  `keterangan_pengeluaran` text
+  `id_user` int(11) NOT NULL,
+  `tanggal` datetime DEFAULT NULL,
+  `kredit` double DEFAULT NULL,
+  `debit` double DEFAULT NULL,
+  `keterangan` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tabel_pengeluaran_project`
+-- Struktur dari tabel `pengeluaran_project`
 --
 
-CREATE TABLE IF NOT EXISTS `tabel_pengeluaran_project` (
+CREATE TABLE IF NOT EXISTS `pengeluaran_project` (
 `id_pengeluaran_project` int(11) NOT NULL,
   `id_anggaran_pengeluaran` int(11) NOT NULL,
   `nama_pengeluaran` varchar(100) DEFAULT NULL,
+  `jumlah_pengeluaran` double NOT NULL,
   `waktu_pengeluaran` datetime DEFAULT NULL,
   `keterangan_pengeluaran` text,
   `bukti_pengeluaran` text,
@@ -92,10 +80,10 @@ CREATE TABLE IF NOT EXISTS `tabel_pengeluaran_project` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tabel_project`
+-- Struktur dari tabel `project`
 --
 
-CREATE TABLE IF NOT EXISTS `tabel_project` (
+CREATE TABLE IF NOT EXISTS `project` (
 `id_project` int(11) NOT NULL,
   `nama_project` varchar(100) DEFAULT NULL,
   `anggaran` double DEFAULT NULL,
@@ -106,10 +94,10 @@ CREATE TABLE IF NOT EXISTS `tabel_project` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tabel_project_personil`
+-- Struktur dari tabel `project_personil`
 --
 
-CREATE TABLE IF NOT EXISTS `tabel_project_personil` (
+CREATE TABLE IF NOT EXISTS `project_personil` (
 `id_project_personil` int(11) NOT NULL,
   `id_project` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
@@ -119,23 +107,10 @@ CREATE TABLE IF NOT EXISTS `tabel_project_personil` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tabel_saldo_pribadi`
+-- Struktur dari tabel `saldo_project`
 --
 
-CREATE TABLE IF NOT EXISTS `tabel_saldo_pribadi` (
-`id_saldo` int(11) NOT NULL,
-  `kredit` double DEFAULT NULL,
-  `debit` double DEFAULT NULL,
-  `id_user` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `tabel_saldo_project`
---
-
-CREATE TABLE IF NOT EXISTS `tabel_saldo_project` (
+CREATE TABLE IF NOT EXISTS `saldo_project` (
 `id_saldo` int(11) NOT NULL,
   `kredit` double DEFAULT NULL,
   `debit` double DEFAULT NULL,
@@ -145,10 +120,10 @@ CREATE TABLE IF NOT EXISTS `tabel_saldo_project` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tabel_tambahan_anggaran`
+-- Struktur dari tabel `tambahan_anggaran`
 --
 
-CREATE TABLE IF NOT EXISTS `tabel_tambahan_anggaran` (
+CREATE TABLE IF NOT EXISTS `tambahan_anggaran` (
 `id_tambahan` int(11) NOT NULL,
   `nama_tambahan` varchar(100) DEFAULT NULL,
   `jumlah_tambahan` double DEFAULT NULL,
@@ -159,10 +134,10 @@ CREATE TABLE IF NOT EXISTS `tabel_tambahan_anggaran` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tabel_user`
+-- Struktur dari tabel `user`
 --
 
-CREATE TABLE IF NOT EXISTS `tabel_user` (
+CREATE TABLE IF NOT EXISTS `user` (
 `id_user` int(11) NOT NULL,
   `nama_user` varchar(100) DEFAULT NULL,
   `alamat` text,
@@ -179,69 +154,57 @@ CREATE TABLE IF NOT EXISTS `tabel_user` (
 --
 
 --
--- Indexes for table `tabel_anggaran_pengeluaran`
+-- Indexes for table `anggaran_pengeluaran`
 --
-ALTER TABLE `tabel_anggaran_pengeluaran`
+ALTER TABLE `anggaran_pengeluaran`
  ADD PRIMARY KEY (`id_anggaran_pengeluaran`), ADD KEY `fk_anggaran_project` (`id_project`);
 
 --
--- Indexes for table `tabel_level_user`
+-- Indexes for table `level_user`
 --
-ALTER TABLE `tabel_level_user`
+ALTER TABLE `level_user`
  ADD PRIMARY KEY (`id_level`);
 
 --
--- Indexes for table `tabel_pendapatan_pribadi`
+-- Indexes for table `pengeluaran_pribadi`
 --
-ALTER TABLE `tabel_pendapatan_pribadi`
- ADD PRIMARY KEY (`id_pendapatan_p`), ADD KEY `fk_pend_user` (`id_user`);
+ALTER TABLE `pengeluaran_pribadi`
+ ADD PRIMARY KEY (`id_pengeluaran_pribadi`), ADD KEY `pengeluaran_user` (`id_user`);
 
 --
--- Indexes for table `tabel_pengeluaran_pribadi`
+-- Indexes for table `pengeluaran_project`
 --
-ALTER TABLE `tabel_pengeluaran_pribadi`
- ADD PRIMARY KEY (`id_pengeluaran_pribadi`), ADD KEY `fk_saldo_pribadi` (`id_saldo_pribadi`);
-
---
--- Indexes for table `tabel_pengeluaran_project`
---
-ALTER TABLE `tabel_pengeluaran_project`
+ALTER TABLE `pengeluaran_project`
  ADD PRIMARY KEY (`id_pengeluaran_project`), ADD KEY `fk_pengeluaran_user` (`id_user`), ADD KEY `fk_pengeluaran_angg` (`id_anggaran_pengeluaran`);
 
 --
--- Indexes for table `tabel_project`
+-- Indexes for table `project`
 --
-ALTER TABLE `tabel_project`
+ALTER TABLE `project`
  ADD PRIMARY KEY (`id_project`);
 
 --
--- Indexes for table `tabel_project_personil`
+-- Indexes for table `project_personil`
 --
-ALTER TABLE `tabel_project_personil`
+ALTER TABLE `project_personil`
  ADD PRIMARY KEY (`id_project_personil`), ADD KEY `fk_pp_user` (`id_user`), ADD KEY `fk_pp_project` (`id_project`), ADD KEY `fk_pp_level` (`id_level_user`);
 
 --
--- Indexes for table `tabel_saldo_pribadi`
+-- Indexes for table `saldo_project`
 --
-ALTER TABLE `tabel_saldo_pribadi`
- ADD PRIMARY KEY (`id_saldo`), ADD KEY `fk_saldopr_user` (`id_user`);
-
---
--- Indexes for table `tabel_saldo_project`
---
-ALTER TABLE `tabel_saldo_project`
+ALTER TABLE `saldo_project`
  ADD PRIMARY KEY (`id_saldo`), ADD KEY `fk_saldo_project` (`id_project`);
 
 --
--- Indexes for table `tabel_tambahan_anggaran`
+-- Indexes for table `tambahan_anggaran`
 --
-ALTER TABLE `tabel_tambahan_anggaran`
+ALTER TABLE `tambahan_anggaran`
  ADD PRIMARY KEY (`id_tambahan`), ADD KEY `fk_tambahan_project` (`id_project`);
 
 --
--- Indexes for table `tabel_user`
+-- Indexes for table `user`
 --
-ALTER TABLE `tabel_user`
+ALTER TABLE `user`
  ADD PRIMARY KEY (`id_user`);
 
 --
@@ -249,114 +212,92 @@ ALTER TABLE `tabel_user`
 --
 
 --
--- AUTO_INCREMENT for table `tabel_anggaran_pengeluaran`
+-- AUTO_INCREMENT for table `anggaran_pengeluaran`
 --
-ALTER TABLE `tabel_anggaran_pengeluaran`
+ALTER TABLE `anggaran_pengeluaran`
 MODIFY `id_anggaran_pengeluaran` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `tabel_level_user`
+-- AUTO_INCREMENT for table `level_user`
 --
-ALTER TABLE `tabel_level_user`
+ALTER TABLE `level_user`
 MODIFY `id_level` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `tabel_pendapatan_pribadi`
+-- AUTO_INCREMENT for table `pengeluaran_pribadi`
 --
-ALTER TABLE `tabel_pendapatan_pribadi`
-MODIFY `id_pendapatan_p` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `tabel_pengeluaran_pribadi`
---
-ALTER TABLE `tabel_pengeluaran_pribadi`
+ALTER TABLE `pengeluaran_pribadi`
 MODIFY `id_pengeluaran_pribadi` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `tabel_pengeluaran_project`
+-- AUTO_INCREMENT for table `pengeluaran_project`
 --
-ALTER TABLE `tabel_pengeluaran_project`
+ALTER TABLE `pengeluaran_project`
 MODIFY `id_pengeluaran_project` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `tabel_project`
+-- AUTO_INCREMENT for table `project`
 --
-ALTER TABLE `tabel_project`
+ALTER TABLE `project`
 MODIFY `id_project` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `tabel_project_personil`
+-- AUTO_INCREMENT for table `project_personil`
 --
-ALTER TABLE `tabel_project_personil`
+ALTER TABLE `project_personil`
 MODIFY `id_project_personil` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `tabel_saldo_pribadi`
+-- AUTO_INCREMENT for table `saldo_project`
 --
-ALTER TABLE `tabel_saldo_pribadi`
+ALTER TABLE `saldo_project`
 MODIFY `id_saldo` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `tabel_saldo_project`
+-- AUTO_INCREMENT for table `tambahan_anggaran`
 --
-ALTER TABLE `tabel_saldo_project`
-MODIFY `id_saldo` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `tabel_tambahan_anggaran`
---
-ALTER TABLE `tabel_tambahan_anggaran`
+ALTER TABLE `tambahan_anggaran`
 MODIFY `id_tambahan` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `tabel_user`
+-- AUTO_INCREMENT for table `user`
 --
-ALTER TABLE `tabel_user`
+ALTER TABLE `user`
 MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Ketidakleluasaan untuk tabel `tabel_anggaran_pengeluaran`
+-- Ketidakleluasaan untuk tabel `anggaran_pengeluaran`
 --
-ALTER TABLE `tabel_anggaran_pengeluaran`
-ADD CONSTRAINT `fk_anggaran_project` FOREIGN KEY (`id_project`) REFERENCES `tabel_project` (`id_project`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `anggaran_pengeluaran`
+ADD CONSTRAINT `fk_anggaran_project` FOREIGN KEY (`id_project`) REFERENCES `project` (`id_project`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Ketidakleluasaan untuk tabel `tabel_pendapatan_pribadi`
+-- Ketidakleluasaan untuk tabel `pengeluaran_pribadi`
 --
-ALTER TABLE `tabel_pendapatan_pribadi`
-ADD CONSTRAINT `fk_pend_user` FOREIGN KEY (`id_user`) REFERENCES `tabel_user` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `pengeluaran_pribadi`
+ADD CONSTRAINT `pengeluaran_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Ketidakleluasaan untuk tabel `tabel_pengeluaran_pribadi`
+-- Ketidakleluasaan untuk tabel `pengeluaran_project`
 --
-ALTER TABLE `tabel_pengeluaran_pribadi`
-ADD CONSTRAINT `fk_saldo_pribadi` FOREIGN KEY (`id_saldo_pribadi`) REFERENCES `tabel_saldo_pribadi` (`id_saldo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `pengeluaran_project`
+ADD CONSTRAINT `fk_pengeluaran_angg` FOREIGN KEY (`id_anggaran_pengeluaran`) REFERENCES `anggaran_pengeluaran` (`id_anggaran_pengeluaran`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_pengeluaran_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Ketidakleluasaan untuk tabel `tabel_pengeluaran_project`
+-- Ketidakleluasaan untuk tabel `project_personil`
 --
-ALTER TABLE `tabel_pengeluaran_project`
-ADD CONSTRAINT `fk_pengeluaran_angg` FOREIGN KEY (`id_anggaran_pengeluaran`) REFERENCES `tabel_anggaran_pengeluaran` (`id_anggaran_pengeluaran`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-ADD CONSTRAINT `fk_pengeluaran_user` FOREIGN KEY (`id_user`) REFERENCES `tabel_user` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `project_personil`
+ADD CONSTRAINT `fk_pp_level` FOREIGN KEY (`id_level_user`) REFERENCES `level_user` (`id_level`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_pp_project` FOREIGN KEY (`id_project`) REFERENCES `project` (`id_project`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_pp_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Ketidakleluasaan untuk tabel `tabel_project_personil`
+-- Ketidakleluasaan untuk tabel `saldo_project`
 --
-ALTER TABLE `tabel_project_personil`
-ADD CONSTRAINT `fk_pp_level` FOREIGN KEY (`id_level_user`) REFERENCES `tabel_level_user` (`id_level`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-ADD CONSTRAINT `fk_pp_project` FOREIGN KEY (`id_project`) REFERENCES `tabel_project` (`id_project`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-ADD CONSTRAINT `fk_pp_user` FOREIGN KEY (`id_user`) REFERENCES `tabel_user` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `saldo_project`
+ADD CONSTRAINT `fk_saldo_project` FOREIGN KEY (`id_project`) REFERENCES `project` (`id_project`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Ketidakleluasaan untuk tabel `tabel_saldo_pribadi`
+-- Ketidakleluasaan untuk tabel `tambahan_anggaran`
 --
-ALTER TABLE `tabel_saldo_pribadi`
-ADD CONSTRAINT `fk_saldopr_user` FOREIGN KEY (`id_user`) REFERENCES `tabel_user` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Ketidakleluasaan untuk tabel `tabel_saldo_project`
---
-ALTER TABLE `tabel_saldo_project`
-ADD CONSTRAINT `fk_saldo_project` FOREIGN KEY (`id_project`) REFERENCES `tabel_project` (`id_project`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Ketidakleluasaan untuk tabel `tabel_tambahan_anggaran`
---
-ALTER TABLE `tabel_tambahan_anggaran`
-ADD CONSTRAINT `fk_tambahan_project` FOREIGN KEY (`id_project`) REFERENCES `tabel_project` (`id_project`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `tambahan_anggaran`
+ADD CONSTRAINT `fk_tambahan_project` FOREIGN KEY (`id_project`) REFERENCES `project` (`id_project`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
